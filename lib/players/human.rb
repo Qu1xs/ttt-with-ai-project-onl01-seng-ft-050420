ef8393c9448bd5e4786a23f42a9d0ddebd8a4bc7
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-18 19:07:28 +0000

    Done.

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,7 +1,10 @@
-class Human
-  
-  
-  
-  
+module Players
+
+  class Human < Player
+    def move(input)
+      puts "Please enter a number from 1 to 9:"
+      input = gets.strip
+    end
+  end
   
 end
