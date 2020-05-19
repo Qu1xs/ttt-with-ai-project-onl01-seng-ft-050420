e05941e92b1219ab2b3ac2db241b156d055b496b
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-19 00:56:50 +0000

    Done.

---
+++ lib/players/computer.rb
@@ -0,0 +1,12 @@
+module Players
+
+  class Computer < Player
+    
+    # def move(input)
+    #   puts "Please enter a number from 1 to 9:"
+    #   input = gets.strip
+    # end
+    
+  end
+  
+end
